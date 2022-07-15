f5ec116b47095d64c749da3909ab7c13e1cc60e4
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-15 20:50:49 +0300

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        System.out.print("Input string -> ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        reverse(1000, input);
+        reverse(10_000, input);
+        reverse(100_000, input);
+
+        scanner.close();
+    }
+
+    private static void reverse(int times, String input) {
+        String reverse = "";
+        System.out.println("Start reversing " + times + " times...");
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            reverse = reverse(input);
+        }
+        System.out.println("Reverse string -> " + reverse + " Time -> " + (System.currentTimeMillis() - startTime));
+    }
+
+    private static String reverse(String input) {
+        StringBuilder stringBuilder = new StringBuilder(input);
+        return stringBuilder.reverse().toString();
+    }
+}
