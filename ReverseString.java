10dd528b4eb33babe0e61aa1f41c07ee03852b99
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-18 22:46:10 +0300

    Initial commit

--- ReverseString.java
+++ ReverseString.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ReverseString {
 
     public static void main(String[] args) {
-        System.out.print("Please input string -> ");
+        System.out.print("Input string -> ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 
